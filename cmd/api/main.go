bb50e1fb888b586d4bdeb8e11a7a1349e2c254b7
Michael <msquaremega@gmail.com>
2023-01-20 14:59:18 +0100

    done

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,7 @@
 package main
 
 import (
-	"encoding/json"
-	"net/http"
-	"twitch_chat_analysis/model"
-
 	"github.com/gin-gonic/gin"
-	"github.com/streadway/amqp"
 )
 
 func main() {
@@ -14,61 +9,6 @@ func main() {
 
 	r.GET("/test", func(c *gin.Context) {
 		c.JSON(200, "worked")
-	})
-
-	r.POST("/message", func(ctx *gin.Context) {
-		var body model.MessageBody
-		err := ctx.ShouldBindJSON(&body)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		conn, err := amqp.Dial("amqp://user:password@rabbitmq:5672/")
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		defer conn.Close()
-
-		ch, err := conn.Channel()
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		defer ch.Close()
-
-		q, err := ch.QueueDeclare(
-			"message_queue",
-			false,
-			false,
-			false,
-			false,
-			nil,
-		)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		bodyBytes, err := json.Marshal(body)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		err = ch.Publish("", q.Name, false, false, amqp.Publishing{
-			ContentType: "application/json",
-			Body:        bodyBytes,
-		})
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		ctx.JSON(http.StatusOK, gin.H{"message": "message sent"})
 	})
 
 	r.Run()
